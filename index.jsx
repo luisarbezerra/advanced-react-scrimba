9043641c4070c854117b931c35fb78590e4751f4
luisarbezerra <luisarbezerra@gmail.com>
2023-08-14 01:53:36 +0200

    add miragejs with api mocked data

--- index.jsx
+++ index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+import './server';
 
 function App() {
   return (
@@ -23,4 +25,4 @@ function App() {
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
