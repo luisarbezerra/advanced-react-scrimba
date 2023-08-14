5eecf13afa01d4c7b8f9a821b989e7f094875bcc
luisarbezerra <luisarbezerra@gmail.com>
2023-08-14 04:01:41 +0200

    update Route with relative paths

--- index.jsx
+++ index.jsx
@@ -5,10 +5,11 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
-import Layout from './components/Layout';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
+import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
 
 import './server';
 
@@ -16,14 +17,19 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          // not needed, but as an example :)
+          <Route path="vans">
+            <Route index element={<Vans />} />
+            <Route path=":id" element={<VanDetail />} />
+          </Route>
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
