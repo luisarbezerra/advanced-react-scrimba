4c097aa5d008ea3656895de5cf3e41c32b1799cb
luisarbezerra <luisarbezerra@gmail.com>
2023-08-15 23:34:55 +0200

    add api.js and loading/error components to Vans

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
+import { getVans } from '../../api';
 
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const typeFilter = searchParams.get('type');
 
   useEffect(() => {
-    fetch('/api/vans')
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
+    async function loadVans() {
+      setLoading(true);
+      try {
+        const data = await getVans();
+        setVans(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadVans();
   }, []);
 
   const displayedVans = typeFilter
@@ -45,6 +58,14 @@ export default function Vans() {
       }
       return prevParams;
     });
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error : {error.message}</h1>;
   }
 
   return (
