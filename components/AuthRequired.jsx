7e4b5687ab83c1310f2a09dfc5586c6470a6bc91
luisarbezerra <luisarbezerra@gmail.com>
2023-08-16 01:20:14 +0200

    add state to navigate when not authenticated

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -4,7 +4,10 @@ import { Navigate, Outlet } from 'react-router-dom';
 export default function AuthRequired() {
   const authenticated = false;
 
-  if (!authenticated) return <Navigate to="/login" />;
+  if (!authenticated)
+    return (
+      <Navigate to="/login" state={{ message: 'You must log in first' }} />
+    );
 
   return <Outlet />;
 }
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const [loginFormData, setLoginFormData] = useState({
     email: '',
     password: '',
   });
+  const location = useLocation();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -22,6 +23,7 @@ export default function Login() {
 
   return (
     <div className="login-container">
+      {location.state?.message && <h3>{location.state.message}</h3>}
       <h1>Sign in to your account</h1>
       <form onSubmit={handleSubmit} className="login-form">
         <input
