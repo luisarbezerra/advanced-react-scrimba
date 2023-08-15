574aa09a1630b21754bfb21b02f9ba398edf666a
luisarbezerra <luisarbezerra@gmail.com>
2023-08-15 18:13:05 +0200

    add host van details page

---
+++ pages/Host/HostVanDetail.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function HostVanDetail() {
+  const { id } = useParams();
+  const [van, setVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [id]);
+
+  if (!van) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <section>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={van.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${van.type}`}>{van.type}</i>
+            <h3>{van.name}</h3>
+            <h4>${van.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
